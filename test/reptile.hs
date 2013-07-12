42bb23d29374919f86e59b3eed243ec807451e05
Simon Beaumont <ix@modelsciences.com>
2013-07-12 18:09:21 +0100

    added some basic timing code for Int32 -> Int32 bitcode functions

--- test/reptile.hs
+++ test/reptile.hs
@@ -1,32 +1,63 @@
+{-# LANGUAGE OverloadedStrings, DeriveDataTypeable #-}
 
 module Main where
 
-import Convert
 import System.Environment(getArgs)
+import System.Console.CmdArgs
+import System.CPUTime
+import Control.Monad (replicateM)
+
+import Text.Printf
+--import Criterion.Main
+--import Criterion.Config
+
 import Data.Int
 import LLVM.Core
 import LLVM.ExecutionEngine
+import Convert
 
+data TestTarget = TestTarget { iterations :: Int
+                             , bitcode :: String
+                             , function :: String
+                             , fnarg :: Int32
+                             } deriving (Show, Data, Typeable)
+                                        
+defaultTestTarget = TestTarget { iterations = 1000
+                               , bitcode = "test.bc"
+                               , function = "testfn"
+                               , fnarg = 10
+                               }
+  
 main :: IO ()
 main = do
   initializeNativeTarget
-  args <- getArgs
-  m <- readBitcodeFromFile (args !! 0)
+  opts <- cmdArgs defaultTestTarget
+
+  -- llvm load and resolve function
+  m <- readBitcodeFromFile (bitcode opts)
   mp <- createModuleProviderForExistingModule m
   funcs <- getModuleValues m
 
   print funcs
   
   let fnn :: Function (Int32 -> IO Int32)
-      Just fnn = lookup "fib" funcs >>= castModuleValue
+      Just fnn = lookup (function opts) funcs >>= castModuleValue
       
   iofn <- runEngineAccess $ do
     addModuleProvider mp
     --generateFunction fnn
     getPointerToFunction fnn
-    
+
+  printf "%s:%s [..%d]\n" (bitcode opts) (function opts) (iterations opts)
   let fn = convert iofn
-  fn (read (args !! 1)) >>= print
-  -- let fn = unsafePurify $ convert iofn
-  -- print (fn $ read (args !! 1))
+  start <- getCPUTime
+  results <- replicateM (iterations opts) (fn (fnarg opts)) 
+  end <- getCPUTime
+
+  let diff = (fromIntegral (end - start) / 10^12)
+      total = sum results
+
+  printf "%s:%s=%d (%0.6fs)\n" (bitcode opts) (function opts) total (diff :: Double)
+  
+
 
