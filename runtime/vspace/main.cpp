8c7c4e45e14ff2465c83785b32a373a0d207aca1
Simon Beaumont <simon@datalligator.com>
2015-01-31 15:33:31 +0000

    Added vpsace runtime implementation

---
+++ runtime/vspace/main.cpp
@@ -0,0 +1,204 @@
+/***************************************************************************
+ * main.cpp symbol table test utility - part of the Gecko qdsm environment.
+ *
+ * Copyright (c) Simon Beaumont 2012-2014 - All Rights Reserved.
+ * See: LICENSE for conditions under which this software is published.
+ ***************************************************************************/
+
+#include <iostream>
+#include <boost/algorithm/string.hpp>
+#include <boost/date_time/posix_time/ptime.hpp>
+#include <boost/date_time/microsec_time_clock.hpp>
+#include <boost/tokenizer.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/program_options.hpp>
+//#include <boost/filesystem.hpp>
+//#include <boost/optional.hpp>
+#include "table.hpp"
+
+
+// wall clock timer
+
+class timer {
+  
+public:
+
+  timer(const std::string& name) : name(name), start(clock_t::local_time()) {}
+
+  const std::size_t get_elapsed_micros() {
+    
+    time_t now(clock_t::local_time());
+    elapsed_t d = now - start;
+    return d.total_microseconds();
+  }
+
+  friend std::ostream& operator<<(std::ostream& os, timer& t) {
+    os << "[" << t.get_elapsed_micros() << "] ";
+    return os;
+  }
+
+private:
+
+  typedef boost::posix_time::ptime time_t;
+  typedef boost::date_time::microsec_clock<time_t> clock_t;
+  typedef boost::posix_time::time_duration elapsed_t;
+  
+  std::string name;
+  time_t start;
+};
+
+
+// tokenzier for input line
+
+void tokenize_command(const std::string& s, std::vector<std::string>& o) {
+  typedef boost::escaped_list_separator<char> ls_t;
+  typedef boost::tokenizer<ls_t> toz_t;
+  
+  ls_t els("\\"," \t","\"\'");
+  toz_t tok(s, els);
+  
+  for(toz_t::iterator j = tok.begin(); j != tok.end(); ++j) {
+    std::string t(*j);
+    boost::trim(t);
+    o.push_back(t);
+  }
+}
+
+// quck and dirty file existence check avoiding boost::system/filesystem libraries
+
+inline bool file_exists(const char *path) {
+  return std::ifstream(path).good();
+}
+
+inline bool file_exists(std::string& path) {
+  return std::ifstream(path).good();
+}
+
+
+////////////////////////////////
+// entry point and command line
+
+int main(int argc, const char** argv) {
+  
+  namespace po = boost::program_options;
+  using gecko::symtab::table;
+
+  std::size_t requested_size;
+  po::options_description desc("Allowed options");
+  po::positional_options_description p;
+  p.add("name", -1);
+
+  desc.add_options()
+    ("help", "Gecko qdsm symbol table test utility")
+    ("size", po::value<std::size_t>(&requested_size)->default_value(0),
+     "requested size for table in Mbytes")
+    ("name", po::value<std::string>(),
+     "name (should be a valid path)");
+
+  po::variables_map opts;
+  po::store(po::command_line_parser(argc, argv).options(desc).positional(p).run(), opts);
+  po::notify(opts);
+  
+  if (opts.count("help")) {
+    std::cout << desc <<  std::endl;
+    return 1;
+  } else if (!opts.count("name")) {
+    std::cout << "table name is required!" << std::endl;
+    return 3;
+  }
+
+  std::string tablename(opts["name"].as<std::string>());
+
+  if (!file_exists(tablename) && requested_size == 0) {
+    std::cout << "cannot create new table: " << tablename
+              << " with: " << requested_size << " bytes" << std::endl;
+    return 5;
+  }
+  
+  // convert to bytes
+  requested_size = requested_size * (1024 * 1024); 
+  
+  // xxx todo sizing and stuff...
+  
+  table mytable(tablename, requested_size);
+  
+  std::cout << mytable << std::endl;
+  
+  int lastindex = mytable.entries();
+  
+  std::string prompt("Ψ> ");
+  std::string input;
+  
+  std::cout << prompt;  
+  // simple command processor
+  while (std::getline(std::cin, input)) {
+
+    boost::trim(input);
+    
+    std::vector<std::string> cv;
+    tokenize_command(input, cv);
+
+    if (cv.size() > 1) {  
+      // assume we have a command
+      
+      // dispatch command
+      if (boost::iequals(cv[0], "=")) {
+
+        // N.B. insertion suceeds even if the symbol already exists in the table
+        // wee need to guard this but not in the fast insert!
+        mytable.insert(cv[1], lastindex++);
+
+        // lookup the inserted symbol
+        if (auto sym = mytable.get_symbol(cv[1]))
+          std::cout << *sym << std::endl;
+        else
+          std::cout << cv[1] << ": not found!" << std::endl;
+        
+      } else if (boost::iequals(cv[0], "<")) {
+        
+        ; // load symbols from file
+        std::ifstream ins(cv[1]);
+        
+        if (ins.good()) {
+          std::string fline;
+          int n = 0;
+          timer mytimer("load timer");
+          
+          while(std::getline(ins, fline)) {
+            boost::trim(fline);
+            mytable.insert(fline, lastindex++);
+            n++;
+          }
+          std::cout << mytimer << " loaded: " << n << std::endl; 
+        } else {
+          std::cout << "can't open: " << cv[0] << std::endl;
+        }
+        
+      } else if (boost::iequals(cv[0], ">")) {
+        ; // export file
+
+      } else if (boost::iequals(cv[0], ".")) {
+        std::cout << "juju" << std::endl;
+
+      } else
+        std::cout << "syntax error:" << input << std::endl;
+
+      
+    } else if (cv.size() > 0) {
+      // default to lookup if no args
+      if (auto sym = mytable.get_symbol(cv[0]))
+        std::cout << *sym << std::endl;
+      else
+        std::cout << cv[0] << ": not found!" << std::endl;
+    }
+    
+    std::cout << prompt;  
+  }
+  
+  // goodbye from me and goodbye from him...
+  std::cout << std::endl << mytable << std::endl << "goodbye" << std::endl;
+  
+  return 0;
+  
+}
+
---
+++ runtime/vspace/table.hpp
@@ -0,0 +1,199 @@
+/***************************************************************************
+ * table.hpp - symbol table (part of the Gecko qdsm environment)
+ *
+ * Copyright (c) Simon Beaumont 2012-2014 - All Rights Reserved.
+ * See: LICENSE for conditions under which this software is published.
+ ***************************************************************************/
+
+#include <iostream>
+#include <boost/interprocess/allocators/allocator.hpp>
+#include <boost/interprocess/managed_mapped_file.hpp>
+#include <boost/interprocess/containers/string.hpp>
+#include <boost/interprocess/containers/vector.hpp>
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/ordered_index.hpp>
+#include <boost/multi_index/hashed_index.hpp>
+#include <boost/multi_index/member.hpp>
+#include <boost/optional.hpp>
+
+
+using boost::multi_index_container;
+using namespace boost::multi_index;
+namespace bip = boost::interprocess;
+
+namespace gecko {
+
+  namespace symtab {
+
+    // memory mapped file based symbol table
+    
+    class table final {
+      
+      // managed segments and allocator types
+      
+      typedef bip::managed_mapped_file                         segment_t;
+      typedef segment_t::segment_manager                       segment_manager_t;
+
+      typedef bip::allocator<void, segment_manager_t>          void_allocator_t;
+      
+      // strings allocated in the segment
+      typedef bip::basic_string<char, std::char_traits<char>, bip::allocator<char, segment_manager_t>> shared_string_t;
+      
+      // vectors of int allocated in the segment
+      typedef bip::allocator<unsigned int, segment_manager_t>  uint_allocator_t;
+      typedef bip::vector<unsigned int, uint_allocator_t>      uint_vector_t;
+      typedef bip::allocator<uint_vector_t, segment_manager_t> uint_vector_allocator_t;
+
+      // id and status 
+      typedef std::size_t id_t;
+      enum status_t {NEW, USED, OLD, FREE};
+
+      // symbols stored in the shared multi_index container
+      
+      struct symbol final {
+        
+        shared_string_t name;
+        id_t id;
+        status_t flags;
+        uint_vector_t bits;
+
+        symbol(const char* s, const id_t& i, const void_allocator_t& void_alloc) : name(s, void_alloc), id(i), flags(NEW), bits(512, 0, void_alloc) {}
+        
+        // constructor requires a shared string
+        //symbol(const shared_string_t& s, const id_t& i) : name(s), id(i), flags(NEW), bits(void_allocator_t) {}
+
+        friend std::ostream& operator<<(std::ostream& os, const symbol& s) {
+          os << "(" << s.name << ", " << s.id << ", " << s.flags << ", " << s.bits.size() << ")";
+          return os;
+        }
+      };
+
+      // symbol allocator
+   
+      typedef bip::allocator<symbol, segment_manager_t> symbol_allocator_t;
+      
+      // shared memory mapped multi index container type with it's indexes
+      
+      typedef multi_index_container<
+        symbol,
+        indexed_by<
+          hashed_unique<BOOST_MULTI_INDEX_MEMBER(symbol, shared_string_t, name)>,
+          hashed_unique<BOOST_MULTI_INDEX_MEMBER(symbol, id_t, id)>
+          >, symbol_allocator_t
+        > symbol_table_t;
+
+      
+      // shared string helpers
+      
+      inline shared_string_t shared_string(const std::string& s) {
+        return shared_string_t(s.c_str(), shared_string_t::allocator_type(allocator));  
+      }
+
+      inline shared_string_t shared_string(const char* s) {
+        return shared_string_t(s, shared_string_t::allocator_type(allocator));  
+      }
+
+      
+    public:
+      
+      // TODO: the segment is a global memory mapped file we need to share this across multiple namespaces
+      // so we need a global singleton to save it in (shared pointer?) and initialize once
+      
+      table(const std::string& s, const size_t& size)
+        : name(s.c_str()), segment(boost::interprocess::open_or_create, "gecko.dat", size), allocator(segment.get_segment_manager()) {
+        // ensure multi_index container is constructed: this is the symbol table
+        db = segment.find_or_construct<symbol_table_t>(name)(allocator);
+      }
+
+
+      ~table() {
+        // should we remove the shared_memory_object (by name) here as well?
+        // segment is global so flushing should be manged by owner... 
+        segment.flush();
+      }
+
+      // delete the rest of the gang don't ever want to copy a table -- but move?
+
+      table(const table&) = delete;
+      table(table&&) = delete;
+      const table& operator=(const table&) = delete;
+      const table& operator=(table&&) = delete;
+
+      // printer
+
+      friend std::ostream& operator<<(std::ostream& os, table& t) {
+        os << "(" << (t.check_sanity() ? ":-) " : ":-( ") << t.tablename()
+           << "[" << t.entries() << "] " <<  t.size() - t.get_free() << "/" << t.get_free() << "=" << t.size() << ")";
+        return os;
+      }  
+
+      // insertion
+      
+      inline void insert(const std::string& k, const id_t& i) {
+        db->insert(symbol(k.c_str(), i, allocator));
+      }
+
+      
+      // xxx not sure how expensive these optional values are at runtime
+
+      // lookup by name
+      
+      inline boost::optional<const symbol&> get_symbol(const char* k) {
+        typedef symbol_table_t::nth_index<0>::type symbol_by_name;
+        
+        symbol_by_name& name_idx = db->get<0>();
+        symbol_by_name::iterator i = name_idx.find(shared_string(k));
+        if (i == name_idx.end()) return boost::none;
+        else return *i;
+      }
+
+      // lookup by name
+
+      inline boost::optional<const symbol&> get_symbol(const std::string& k) {
+        typedef symbol_table_t::nth_index<0>::type symbol_by_name;
+        
+        symbol_by_name& name_idx = db->get<0>();
+        symbol_by_name::iterator i = name_idx.find(shared_string(k));
+        if (i == name_idx.end()) return boost::none;
+        else return *i;
+      }
+
+      // lookup by index
+
+      inline boost::optional<const symbol&> get_symbol(const id_t& k) {
+        typedef symbol_table_t::nth_index<1>::type symbol_by_id;
+        
+        symbol_by_id& id_idx = db->get<1>(); 
+        symbol_by_id::iterator i = id_idx.find(k);
+        if (i == id_idx.end()) return boost::none;
+        else return *i;
+      }
+
+      // XXX todo prefix btree/rb index lookup
+      
+      // delegated iterators
+
+      typedef symbol_table_t::iterator iterator;
+      typedef symbol_table_t::const_iterator const_iterator;
+      
+      inline iterator begin() { return db->begin(); }
+      inline iterator end() { return db->end(); }
+
+      // delegated properties
+      
+      inline const size_t size() { return segment.get_size(); }
+      inline const size_t get_free() { return segment.get_free_memory(); }
+      inline const bool check_sanity() { return segment.check_sanity(); }
+      inline const size_t entries() { return db->size(); }
+      inline const char* tablename() const { return name; }
+      // TODO: shrink_to_fit, grow
+      
+    private:    
+      const char*          name; 
+      symbol_table_t*      db;
+      segment_t            segment;
+      void_allocator_t     allocator;
+    };
+    
+  }
+}
