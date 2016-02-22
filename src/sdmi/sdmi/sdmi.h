27577934e1322426a0a22a850fb21750d0732a6a
Simon Beaumont <s@molemind.net>
2016-02-22 15:40:55 +0000

    Added sdmi iOS Framework xcode project.

---
+++ src/sdmi/sdmi/sdmi.h
@@ -0,0 +1,19 @@
+//
+//  sdmi.h
+//  sdmi
+//
+//  Created by Simon Beaumont on 22/02/2016.
+//  Copyright © 2016 Simon Beaumont. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for sdmi.
+FOUNDATION_EXPORT double sdmiVersionNumber;
+
+//! Project version string for sdmi.
+FOUNDATION_EXPORT const unsigned char sdmiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sdmi/PublicHeader.h>
+
+
