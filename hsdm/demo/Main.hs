e6025abb3fc8f010680e9b55bf1a0e97fa8d585d
Simon Beaumont <s@molemind.net>
2017-04-11 19:51:01 +0100

    Added demo visualisation server.

---
+++ hsdm/demo/Main.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Main where
+
+
+import Control.Concurrent
+import Data.Time
+import Diagrams.Backend.Canvas
+import Diagrams.Prelude
+import Graphics.Blank hiding (rotate, scale, square, ( # ))
+
+-- generates a diagram from time
+
+clock :: UTCTime -> Diagram B
+clock t = circle 0.35 # fc silver # lwG 0 
+          <> bigHand # f 12 h
+          <> littleHand # f 60 m
+          <> secondHand # f 60 (q s)
+          <> circle 1  # fc black # lwG 0
+          <> circle 11 # lwG 1 # lc white # fc lightsteelblue # pad 1.1
+  where
+    s = realToFrac $ utctDayTime t :: Double
+    m = s / 60
+    h = m / 60
+    
+    bigHand    = (0 ^& (-1.5)) ~~ (0 ^& 7.5) # lwG 0.5
+    littleHand = (0 ^& (-2))   ~~ (0 ^& 9.5) # lwG 0.2
+    secondHand = (0 ^& (-1.5)) ~~ (0 ^& 10.0) # lwG 0.1
+    
+    f n v = rotate (- v / n @@ turn)
+    q :: Double -> Double
+    q = realToFrac . round
+
+main :: IO ()
+main = blankCanvas 3000 $ \context -> loop context
+
+loop :: DeviceContext -> IO a
+loop context = do
+  tn <- getCurrentTime
+  send context $ renderDia Canvas (CanvasOptions (mkWidth 200))
+                                  (clock tn)
+  threadDelay 1000000 -- nice animation of second hand
+  loop context
--- hsdm/size_t.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-int main() {
-  printf("%lu\n", sizeof(size_t));
-  printf("%lu\n", sizeof(unsigned long));
-  return 0;
-}
